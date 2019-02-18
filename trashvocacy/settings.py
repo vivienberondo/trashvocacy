0eb72299808c0de73a88779f90466359bd5cdb5a
Keisha Louise Vivien Berondo <berondovivien@gmail.com>
2019-02-18 21:07:41 +0800

    Modified settings.py

--- trashvocacy/settings.py
+++ trashvocacy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9=edreyqx&=)ujljzxn6gaxo@wd9n16k#k3$nhjj8j179t$vj8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'reservation',  # online reservation
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
